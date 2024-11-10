99a147e0531beb850e1d3e1d4bb125f576ea5daa
SaadAslam58 <saadxpert58@gmail.com>
2024-11-10 17:28:15 +0500

    sixth commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import { Socials } from "@/constants";
-import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div className="nav-container">
